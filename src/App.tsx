562d5f687c802e0216820b7b855faa60b8c0e35b
Ramanujam Gond <ramanujamgond@gmail.com>
2024-07-08 18:03:41 +0530

    feat: install the axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import UserSignup from "./UserSignup";
 import Wheel from "./Wheel";
 
 function App() {
+  const [formState, setFormState] = useState(true);
+  const [wheelState, setWheelState] = useState(false);
+
+  // Save formState to localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("form", JSON.stringify(formState));
+    localStorage.setItem("wheel", JSON.stringify(wheelState));
+  }, [formState, wheelState]);
+
   return (
     <>
-      <Wheel />
-      <UserSignup />
+      {formState && <UserSignup />}
+      {wheelState && <Wheel />}
     </>
   );
 }
